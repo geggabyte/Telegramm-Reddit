07cf66a8c5bcf076556251c8c25a0aab7c163d9f
Hennadii Kolesnikov <74913520+geggabyte@users.noreply.github.com>
2024-04-15 16:45:41 +0200

    Update v2

--- update.sh
+++ update.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 echo "This will remove .db and subreddits file. I reccomend you to copy those first"
-read -p "Are you sure? " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-fi
-
-git restore .
-git pull
+read -p "Continue (y/n)?" choice
+case "$choice" in 
+  y|Y ) git restore . & git pull;;
+  n|N ) echo "Good";;
+  * ) echo "Invalid";;
+esac
